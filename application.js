8d7254184087fed13844418001c88fe9da12e205
pilip <filip@scalacalc.u>
2020-09-01 15:43:56 +0300

    email viewer

--- application.js
+++ application.js
@@ -1,23 +1,51 @@
+const fs = require('fs');
 const app = require('express')();
-const PORT = 3000;
 
-const Mbox = require('node-mbox');
 const simpleParser = require('mailparser').simpleParser;
+const Mbox = require('node-mbox');
 
-const mbox = new Mbox();
-process.stdin.pipe(mbox);
+let emails = {};
 
-const emails = [];
+const parseMboxFile = filename => {
+    emails = {};
 
-mbox.on('message', async raw => {
-    const email = await simpleParser(raw);
-    emails.push(email.textAsHtml);
+    const mbox = new Mbox(filename);
+
+    mbox.on('message', async raw => {
+        const email = await simpleParser(raw);
+        const to = email.to.value[0].address;
+
+        if (!emails[to]) {
+            emails[to] = [];
+        }
+
+        emails[to].push(email.textAsHtml);
+    });
+};
+
+const FILENAME = process.argv[3];
+fs.watchFile(FILENAME, () => {
+    console.log('file changed!');
+    parseMboxFile(FILENAME);
 });
+parseMboxFile(FILENAME);
 
 app.get('/', (req, res) => {
-    res.send(`${emails.map(email => `<div>${email}</div>`).join('')}`);
+    res.send(`<h3>Usage: </h3><p>Append a email address to the url to see all emails for that recipient.<br>Example: bots.godsss.online/manugay1@gmail.com</p>`);
 });
 
+app.get('/:email', (req, res) => {
+    if (!emails[req.params.email]) {
+        return res.send(`<h3>There are no emails for ${req.params.email}.</h3><p>Try searching for another address.</p>`)
+    }
+
+    const addressEmails = [...emails[req.params.email]];
+
+    addressEmails.reverse();
+    res.send(`${emails[req.params.email].map(email => `<div>${email}</div>`).join('')}`);
+});
+
+const PORT = +process.argv[2];
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
